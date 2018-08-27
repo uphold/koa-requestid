a7f5856bc688cab1a1c2168c3fe12594da8c98ea
Steven McDowall <sjm@hawoods.org>
2018-08-27 08:57:55 -0400

    Add Typescript types file definition
    Add a little Typescript index.d.ts definition file to make TS happy when used in a TS project.

---
+++ index.d.ts
@@ -0,0 +1,13 @@
+// Type definitions for koa-requestid 2.0.1
+// Project: https://github.com/uphold/koa-requestid
+// Definitions by: Steven McDowall <https://github.com/sjmcdowall>
+//
+// TypeScript Version: 2.3
+import { Middleware } from 'koa';
+
+declare function requestId(options?: {
+  expose?: string;
+  header?: string;
+  query?: string;
+}): Middleware;
+export = requestId;
