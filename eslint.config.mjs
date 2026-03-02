8d434ba71cba36d894e020c5b5c808fde5a7da94
Rafael Santos <rafael.santos@uphold.com>
2026-03-02 13:25:11 +0000

    Replace `jest` with `node:test`
    Signed-off-by: Rafael Santos <rafael.santos@uphold.com>

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,21 +2,10 @@
  * Module dependencies.
  */
 
-import { defineConfig } from 'eslint/config';
-import globals from 'globals';
 import upholdConfig from 'eslint-config-uphold';
 
 /**
  * `ESLint` configuration.
  */
 
-export default defineConfig([
-  upholdConfig,
-  {
-    files: ['test/**/*.js'],
-    languageOptions: {
-      globals: globals.jest
-    },
-    name: 'koa-requestid/tests'
-  }
-]);
+export default upholdConfig;
